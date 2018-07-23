83a9cad836bc341aa4a3d122d09a6993bf557786
dixsie <dwolm001@gmail.com>
2018-07-22 20:01:51 -0400

    corrected  association between link and user

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,4 +1,5 @@
 class ForumsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_forum, only: [:show, :edit, :update, :destroy]
 
   # GET /forums
