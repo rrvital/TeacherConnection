ff7e8c52f6a4f5b72a008ed335f0827e2374afda
dixsie <dwolm001@gmail.com>
2018-07-25 11:53:20 -0400

    removed react javascript folder

--- app/javascript/packs/App.js
+++
@@ -1,9 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-import Forums from '../bundles/App/Forums';
-
- 
-// This is how react_on_rails can see the App in the browser.
-ReactOnRails.register({
-  Forums,
-
-});
