52b1203730b1387549fb4ea77daa8d278ba22f23
dixsie <dwolm001@gmail.com>
2018-07-26 14:03:07 -0400

    fixed sign in issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,10 +62,10 @@
 #         end
 #     end
 # end
-# User.destroy_all
+User.destroy_all
 
 5.times do |i|
-            User.create!([{
+            User.create(!([{)
             first_name: Faker::Name.first_name , 
             last_name: Faker::Name.last_name, 
             email: Faker::Internet.email, 
