14e3fe7c9a52b78e2ba07d33d44bd30c47c022c4
dixsie <dwolm001@gmail.com>
2018-07-30 15:37:44 -0400

    scaffolded events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   devise_for :users, controllers: { registrations: 'user/registrations' }  
   
   devise_scope :user do 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_181449) do
+ActiveRecord::Schema.define(version: 2018_07_30_191014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2018_07_27_181449) do
     t.datetime "updated_at", null: false
     t.index ["forum_id"], name: "index_comments_on_forum_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "address"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "forums", force: :cascade do |t|
